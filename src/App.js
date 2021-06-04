6e7cb2f7e81336ce433ce291127f9d7f4336a0ff
babyazalea <usun12@hotmail.com>
2021-06-05 01:39:24 +0900

    removed router & added useAuth

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
+import { Route, Switch } from "react-router-dom";
 
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { useAuth } from "./hooks/auth-hook";
 
 import Home from "./components/Home/Home";
 import Burger from "./components/Burger/Burger";
@@ -9,55 +10,67 @@ import Auth from "./components/Auth/Auth";
 import Layout from "./components/Layout/Layout";
 import User from "./components/User/User";
 
-let routes = (
-  <Switch>
-    <Route path="/" exact>
-      <Home />
-    </Route>
-    <Route path="/burger-maker">
-      {/* <Burger
-                  isLoggedIn={isLoggedIn}
-                  isVerified={isVerified}
-                  userId={userId}
-                /> */}
-      <Burger />
-    </Route>
-    {/* <Route path="/users/reset-password" exact>
-                <ResetPassword notLoggedIn={true} />
-              </Route> */}
-    {/* <Route path="/users/:id/reset-password">
-                <ResetPassword userEmail={userEmail} />
-              </Route> */}
-    <Route path="/credits" exact>
-      <Credits />
-    </Route>
-    <Route path="/users">
-      {/* <UserProfile
-                  token={token}
-                  userId={userId}
-                  userName={userName}
-                  userEmail={userEmail}
-                  photoUrl={photoUrl}
-                  updateProfile={updateProfile}
-                /> */}
-      <User />
-    </Route>
-    <Route path="/auth" exact>
-      <Auth />
-    </Route>
-    {/* <Route path="/auth/signup" component={Signup} exact />
-              <Route path="/auth/google/:token">
-                <AuthWithGoogle googleLogin={googleLogin} />
-              </Route> */}
-  </Switch>
-);
-
 const App = () => {
+  const {
+    token,
+    userName,
+    userEmail,
+    photoUrl,
+    userId,
+    isLoggedIn,
+    isVerified,
+    login,
+    googleLogin,
+    updateProfile,
+    logout,
+  } = useAuth();
+
+  let routes = (
+    <Switch>
+      <Route path="/" exact>
+        <Home />
+      </Route>
+      <Route path="/burger-maker">
+        {/* <Burger
+                    isLoggedIn={isLoggedIn}
+                    isVerified={isVerified}
+                    userId={userId}
+                  /> */}
+        <Burger />
+      </Route>
+      {/* <Route path="/users/reset-password" exact>
+                  <ResetPassword notLoggedIn={true} />
+                </Route> */}
+      {/* <Route path="/users/:id/reset-password">
+                  <ResetPassword userEmail={userEmail} />
+                </Route> */}
+      <Route path="/credits" exact>
+        <Credits />
+      </Route>
+      <Route path="/users">
+        {/* <UserProfile
+                    token={token}
+                    userId={userId}
+                    userName={userName}
+                    userEmail={userEmail}
+                    photoUrl={photoUrl}
+                    updateProfile={updateProfile}
+                  /> */}
+        <User />
+      </Route>
+      <Route path="/auth">
+        <Auth login={login} googleLogin={googleLogin} />
+      </Route>
+      {/* <Route path="/auth/signup" component={Signup} exact />
+                <Route path="/auth/google/:token">
+                  <AuthWithGoogle googleLogin={googleLogin} />
+                </Route> */}
+    </Switch>
+  );
+
   return (
     <div className="App" id="App">
-      <Router>
-        <Layout>{routes}</Layout>
-      </Router>
+      <Layout userName={userName}>{routes}</Layout>
     </div>
   );
 };
