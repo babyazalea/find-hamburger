f8e1fcbadad8311611dc534906b46a437f5ad4ae
babyazalea <usun12@hotmail.com>
2021-06-08 01:27:53 +0900

    fontawesome 패키지 사용

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,11 +1,9 @@
 import React from "react";
 
-import { icons } from "../../assets/icons/icons";
-
 const Home = () => {
   return (
     <div className="find-hamburger__home">
-      <h1>{icons("home")}</h1>
+      <i className="fas fa-home"></i>
     </div>
   );
 };
