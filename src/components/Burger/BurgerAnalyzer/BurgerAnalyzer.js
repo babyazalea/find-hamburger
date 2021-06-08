dfa1883b1b0b92e6cc7f51b2d6fd144f663495ff
babyazalea <usun12@hotmail.com>
2021-06-08 14:25:49 +0900

    loading-dots 추가

--- src/components/Burger/BurgerAnalyzer/BurgerAnalyzer.js
+++ src/components/Burger/BurgerAnalyzer/BurgerAnalyzer.js
@@ -1,32 +1,41 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 import MyBurger from "./MyBurger/MyBurger";
 import AnalysisResult from "./AnalysisResult/AnalysisResult";
+import LoadingDots from "../../UI/LoadingDots/LoadingDots";
 
 import { burgerRanker } from "../../../utils/burger-ranker";
 
 import "./BurgerAnalyzer.css";
 
 const BurgerAnalyzer = (props) => {
-  const scoreBoard = burgerRanker(props.ingredients);
+  const [scoreBoard, setScoreBoard] = useState(null);
 
-  const sortedScoreBoard = scoreBoard
-    .sort(function (a, b) {
-      if (a.score > b.score) {
-        return -1;
-      }
+  useEffect(() => {
+    setScoreBoard(burgerRanker(props.ingredients));
+  }, [props.ingredients]);
 
-      if (a.score < b.score) {
-        return 1;
-      }
+  let sortedScoreBoard;
 
-      return 0;
-    })
-    .slice(0, 3);
+  if (scoreBoard) {
+    sortedScoreBoard = scoreBoard
+      .sort(function (a, b) {
+        if (a.score > b.score) {
+          return -1;
+        }
+
+        if (a.score < b.score) {
+          return 1;
+        }
+
+        return 0;
+      })
+      .slice(0, 3);
+  }
 
   let analyzer = <p>not...ok</p>;
 
-  if (props.isAnalyzed && sortedScoreBoard.length) {
+  if (props.isAnalyzed && sortedScoreBoard) {
     analyzer = sortedScoreBoard.map((burgerResult, index) => {
       return (
         <AnalysisResult
@@ -42,10 +51,14 @@ const BurgerAnalyzer = (props) => {
 
   return (
     <div className="burger__analyzer">
-      <div className="result__board">
-        <MyBurger ingredients={props.ingredients} />
-        <ul>{analyzer}</ul>
-      </div>
+      {scoreBoard ? (
+        <div className="result__board">
+          <MyBurger ingredients={props.ingredients} />
+          <ul>{analyzer}</ul>
+        </div>
+      ) : (
+        <LoadingDots />
+      )}
     </div>
   );
 };
