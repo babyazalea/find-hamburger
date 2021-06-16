7e15a42775ef0ef129e5792fefec045923164d21
babyazalea <usun12@hotmail.com>
2021-06-16 23:17:28 +0900

    크레딧 페이지 추가

--- src/components/Credits/Credits.js
+++ src/components/Credits/Credits.js
@@ -1,9 +1,28 @@
 import React from "react";
 
+import "./Credits.css";
+
 const Credits = () => {
   return (
-    <div className="find-hamburger__credits">
-      <h1>Credits</h1>
+    <div className="credits">
+      <article>
+        <p>
+          <span>만든 사람:</span>babyazalea
+        </p>
+        <p>
+          <span>깃헙:</span>
+          <a
+            href="https://github.com/babyazalea"
+            rel="noopener noreferrer"
+            target="_blank"
+          >
+            https://github.com/babyazalea
+          </a>
+        </p>
+        <p>
+          <span>이메일:</span>usun12@hotmail.com
+        </p>
+      </article>
     </div>
   );
 };
